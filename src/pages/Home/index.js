8b7e8560f229e1059cf78cdecf5497bbc9bbe91d
Gabriel Luciano <gomeslucianogabriel@gmail.com>
2020-08-29 16:22:45 -0300

    Template página home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
+import Button from '../../components/Button';
+import DefaultPage from '../../components/DefaultPage';
+
+import './styles.css';
+
 function Home() {
-  return (
-    <div className="home-container">
-      HOME PAGE
-    </div>
+  return (   
+    <DefaultPage>
+      <div className="button-container">
+        <Button 
+          onClick={() => console.log("CLICKOU 1")}
+          buttonText="Questão 1"      
+        />
+
+        <Button 
+          onClick={() => console.log("CLICKOU 2")}
+          buttonText="Questão 2"      
+        />
+
+        <Button 
+          onClick={() => console.log("CLICKOU 3")}
+          buttonText="Questão 3"      
+        />
+      </div> 
+    </DefaultPage>
   );
 }
 
