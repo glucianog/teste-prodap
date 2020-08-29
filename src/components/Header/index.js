3c2da57df6826ac614098203051ee9fc47f7cc03
Gabriel Luciano <gomeslucianogabriel@gmail.com>
2020-08-29 16:11:17 -0300

    Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+function Header() {
+  return (
+    <header>
+      <div className="header-content">
+        <strong> Desafio Técnico - Prodap </strong>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
